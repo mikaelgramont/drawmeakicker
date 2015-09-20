bf500e50eacc515bf39257f35b419d8c6dd4d5da
mikaelgramont <mgramont@gmail.com>
2015-09-20 14:25:33 -0700

    Removing constants.php from repo.

--- constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DEFAULT_OG_IMAGE', 'default-kicker.png');
-define('UPLOAD_DIR', 'uploads/');
-define("SITE_TITLE", "Build it. Huck it.");
-define("SITE_URL", "http://www.local.dev/");
-define("VIDEO_ID", "rE5O35mUvBg");
-define("OG_DESCRIPTION", "Design your next kicker in 3D.");
