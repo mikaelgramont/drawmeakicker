b3d26658ce33cf02a08a41dcc0c917fb8edea12c
mikaelgramont <mgramont@gmail.com>
2015-10-11 15:37:16 -0700

    Added utm tag for sharing tracking.

--- php/share.php
+++ php/share.php
@@ -1,11 +1,13 @@
 <?php
 class Share {
 	public static function facebook($url) {
+		$url .= "&utm=facebook";
 		$encoded = urlencode($url);
 		return "https://www.facebook.com/sharer.php?u=".$encoded;
 	}
 
 	public static function twitter($url, $title, $description) {
+		$url .= "&utm=twitter";
 		$encoded = urlencode($url);
 		$shareUrl = "https://twitter.com/intent/tweet";
 
