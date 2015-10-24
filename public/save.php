43cee16da1db5e818fee462a9c6e8fff920e49df
mikaelgramont <mgramont@gmail.com>
2015-10-23 19:58:13 -0700

    Whoops, looks like I broke the save function when I introduced the caching layer.

--- public/save.php
+++ public/save.php
@@ -1,18 +1,23 @@
 <?php
 set_include_path('../php');
 require("constants.php");
+require('Zend/Loader.php');
+require('Zend/Cache.php');
+require('Zend/Exception.php');
+require("cache.php");
 require("dbsettings.php");
 require("imageutil.php");
 require("kickerdao.php");
 require("opengraph.php");
 
+$cache = Cache::getCache(CACHE_METHOD);
 $kickerData = null;
 try {	
 	$dbh = KickerDao::getDb();
 	list($errors, $id) = KickerDao::create($_POST, $dbh);
 	$status = $id ? "success" : "error";
 	if ($id) {
-		$kickerData = KickerDao::loadById($id, $dbh);
+		$kickerData = KickerDao::loadById($id, $cache);
 	}
 } catch (Exception $e) {
 	$status = "error";
