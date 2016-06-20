bca86ea4f62eb4202c9f80af2823cb51442dc913
Mikael Gramont <mgramont@gmail.com>
2016-06-19 22:22:15 -0700

    Fixed bug where image export would only show 1/4 of the actual content on hi-res displays.

--- public/scripts/mergedrenderer.js
+++ public/scripts/mergedrenderer.js
@@ -6,20 +6,21 @@ var MergedRenderer = function(canvas3dEl, canvas2dEl, mergedCanvasEl) {
 
 MergedRenderer.prototype.resize = function() {
 	var parent = this.mergedCanvasEl.parentElement;
-	this.mergedCanvasEl.width = parent.clientWidth;
-	this.mergedCanvasEl.height = parent.clientHeight;
+	this.mergedCanvasEl.width = parent.clientWidth * window.devicePixelRatio;
+	this.mergedCanvasEl.height = parent.clientHeight * window.devicePixelRatio;
+
 };
 
 MergedRenderer.prototype.getData = function(options) {
 	var ctx = this.mergedCanvasEl.getContext('2d');
-	ctx.clearRect(0, 0, this.mergedCanvasEl.width, this.mergedCanvasEl.height);          
+	ctx.clearRect(0, 0, this.mergedCanvasEl.width, this.mergedCanvasEl.height);
 	if (options.fill) {
 		ctx.fillStyle = '#3B69D5';
 		ctx.fillRect(0, 0, this.mergedCanvasEl.width, this.mergedCanvasEl.height);          
 	}
 	ctx.drawImage(this.canvas3dEl, 10, 10);
 	if (options.borders) {
-		ctx.drawImage(this.canvas2dEl, 10, 10);
+		ctx.drawImage(this.canvas2dEl, 10, 10, this.mergedCanvasEl.width - 20, this.mergedCanvasEl.height - 20);
 	}
 
 	return this.mergedCanvasEl.toDataURL();
